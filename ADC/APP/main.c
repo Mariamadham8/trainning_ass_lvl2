f19acaf15e8517ced04c82192d4c584032e6c1d7
mariam_adham <121618861+Mariamadham8@users.noreply.github.com>
2025-02-18 05:50:08 +0200

    ADD_LM35_Sensor

--- ADC/APP/main.c
+++ ADC/APP/main.c
@@ -1,12 +1,14 @@
 /*
  * main.c
  *
- *  Created on: Feb 13, 2025
+ *  Created on: Feb 18, 2025
  *      Author: Mariem
  */
 
+
 #include"../LIB/stdTypes.h"
 #include"../LIB/errorStates.h"
+#include <stdlib.h>
 
 #include"../MCAL/EXTI/EXTI_int.h"
 #include"../MCAL/EXTI/EXTI_config.h"
@@ -19,22 +21,27 @@
 
 #include"../MCAL/GIE/GIE_int.h"
 
+
+#include"../HAL/LCD/LCD_int.h"
+#include"../HAL/LCD/LCD_config.h"
+
 extern EXTI_t  EXTI_AstrEXTIConfig[3];
 
-
+u8  flag=0;
 void read(void *Param)
 {
 	ADC_enuReadValue((u16*)Param);
+	flag=1;
 }
 int main()
 {
-	    u8 adcread=0;
-	    u16 adc16=0;
-		enuSetPinDirection(DIO_PORTA,DIO_PIN2,INPUT);
+	    char adcString[100];  // Buffer to hold ADC value as a string (4 digits + null terminator)
+	    u16 adcread=0;
+	    enuSetPinDirection(DIO_PORTA,DIO_PIN2,INPUT);
 		enuSetPortDirection(DIO_PORTC,0xff);
 		enuSetPinDirection(DIO_PORTD,DIO_PIN2,INPUT);
 
-
+        //conversion will only happen when falling edge in the INT0 pin
 		EXTI_enuinit(EXTI_AstrEXTIConfig);
 		EXTI_enuSetSenceMOde(0,Falling_edge);
 		EXTI_enuEnableINT(0);
@@ -48,15 +55,18 @@ int main()
 		ADC_enuStartConversion();
 
 		GIE_enuEnable();
+		LCD_enuInit();
 
 
 		while(1){
-			//Polling
 
-			//ADC_enuStartConversion();
-			//ADC_enuPollingSystem();
-			//ADC_enuReadHighValue(&adcread);
-			enuSetPortValue(DIO_PORTC,adcread);
+			if(flag==1)
+			{
+					itoa(adcread, adcString, 10);  // Convert ADC value to a string in base 10
+					LCD_enuSendCommand(0x01);      // Clear the LCD before displaying new value
+					LCD_enuSendString(adcString);  // Display the string on the LCD
+					flag = 0;
+			}
 
 		}
 
